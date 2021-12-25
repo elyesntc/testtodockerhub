c8edd288fa789fa64402450d9227e9b64fbe6b50
elyesntc <61362019+elyesntc@users.noreply.github.com>
2021-12-25 20:13:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var os = require("os");
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('<center>Formation présentée par Elyes Gassara<br>Liaison entre GitHub et DockerHub <font color="0000FF"><b>version 1</b> </font><br> Conteneur : <b>' + String(os.hostname()));
+res.send('<center>Formation présentée par Elyes Gassara<br>Liaison entre GitHub et DockerHub <font color="0000FF"><b>version 2</b> </font><br> Conteneur : <b>' + String(os.hostname()));
 });
 app.listen(PORT, HOST);
 console.log(String(os.hostname()));
